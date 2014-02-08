b3a243cc2a8039c73224a07634141ff72c3dad98
xoxfaby <xoxfaby@gmail.com>
2014-02-08 02:08:36 +0100

    Updates

--- FixYourName.lua
+++ FixYourName.lua
@@ -6,8 +6,14 @@ function PLUGIN:Init()
 	local b, res = config.Read( "FixYourName" )
 	self.Config = res or {}
 	self:LoadDefaultConfig()
-	config.Save( "FixYourName" )	
 	self.Config.Language = loadLocalization(self.Config.Language)
+	config.Save( "FixYourName" )	
+	
+	
+	self.Config.BannedNames = {}
+	for i in string.gmatch(self.Config.Names, "[^%;]+") do
+		self.Config.BannedNames[#self.Config.BannedNames + 1] = i
+	end
 end
 
 function loadLocalization(setting)
@@ -17,21 +23,46 @@ function loadLocalization(setting)
 	LocalTxt["english"] = {}
 	LocalTxt["english"].kickChatColor = " has been kicked ( Color code ) "
 	LocalTxt["english"].kickNoticeColor = "Kicked: Color code"
-	LocalTxt["english"].kickChatCharacter = " has been kicked ( Name already in use  "
-	LocalTxt["english"].kickNoticeCharacter = "Kicked: Name already in use"
-	LocalTxt["english"].kickChatDuplicate = " has been kicked ( Name contains illegal character ) "
-	LocalTxt["english"].kickNoticeDuplicate = "Kicked: Name contains illegal character"	
+	LocalTxt["english"].kickChatDuplicate = " has been kicked ( Name already in use ) "
+	LocalTxt["english"].kickNoticeDuplicate = "Kicked: Name already in use"
+	LocalTxt["english"].kickChatCharacter = " has been kicked ( Name contains illegal character ) "
+	LocalTxt["english"].kickNoticeCharacter = "Kicked: Name contains illegal character"	
 	
 	LocalTxt["german"] = {}
 	LocalTxt["german"].kickChatColor = " wurde gekickt ( Farb-code ) "
 	LocalTxt["german"].kickNoticeColor = "Gekickt: Farb-code"
-	LocalTxt["german"].kickChatCharacter = " wurde gekickt ( Name wird schon verwendet )  "
-	LocalTxt["german"].kickNoticeCharacter = "Gekickt: Name wird schon verwendet "
-	LocalTxt["german"].kickChatDuplicate = " wurde gekickt ( Unerlaubtes symbol ) "
-	LocalTxt["german"].kickNoticeDuplicate = "Gekickt: Unerlaubtes symbol"
+	LocalTxt["german"].kickChatDuplicate = " wurde gekickt ( Name wird schon verwendet )  "
+	LocalTxt["german"].kickNoticeDuplicate = "Gekickt: Name wird schon verwendet "
+	LocalTxt["german"].kickChatCharacter = " wurde gekickt ( Unerlaubtes symbol ) "
+	LocalTxt["german"].kickNoticeCharacter = "Gekickt: Unerlaubtes symbol"
 	LocalTxt["deutsch"] = LocalTxt["german"]
 	
-	LocalTxt.Allowed = { "english", "german", "deutsch" }
+	LocalTxt["dutch"] = {}
+    LocalTxt["dutch"].kickChatColor = " is gekicked ( Naam bevat kleurcode ) "
+    LocalTxt["dutch"].kickNoticeColor = "Gekicked: Naam bevat kleurcode"
+    LocalTxt["dutch"].kickChatDuplicate = " is gekicked ( Naam al in gebruik )  "
+    LocalTxt["dutch"].kickNoticeDuplicate = "Gekicked: Naam al in gebruik "
+    LocalTxt["dutch"].kickChatCharacter = " is gekicked ( Naam bevat verboden karakters ) "
+    LocalTxt["dutch"].kickNoticeCharacter = "Gekicked: Naam bevat verboden karakters"
+    LocalTxt["nederlands"] = LocalTxt["dutch"]
+	
+	LocalTxt["portuguese"] = {}
+	LocalTxt["portuguese"].kickChatColor = " foi kickado ( Color code ) "
+	LocalTxt["portuguese"].kickNoticeColor = "kickado: Color code"
+	LocalTxt["portuguese"].kickChatDuplicate = " foi kickado ( Nome ja esta em uso "
+	LocalTxt["portuguese"].kickNoticeDuplicate = "kickado: Nome ja esta em uso"
+	LocalTxt["portuguese"].kickChatCharacter = " foi kickado ( Nome contem caracteres ilegais ) "
+	LocalTxt["portuguese"].kickNoticeCharacter = "kickado: Nome contem caracteres ilegais"
+	
+	LocalTxt["pt-br"] = {}
+	LocalTxt["pt-br"].kickChatColor = " foi kickado ( usando codigo de cor ) "
+	LocalTxt["pt-br"].kickNoticeColor = "kickado: usando codigo de cor"
+	LocalTxt["pt-br"].kickChatDuplicate = " foi kickado ( Este nome ja esta em uso )"
+	LocalTxt["pt-br"].kickNoticeDuplicate = "Kickado: Este nome ja esta em uso"
+	LocalTxt["pt-br"].kickChatCharacter = " foi kickado ( Nome contem caracteres ilegais ) "
+	LocalTxt["pt-br"].kickNoticeCharacter = "Kickado: Nome contem caracteres ilegais"
+	
+	LocalTxt.Allowed = { "english", "german", "deutsch", "dutch", "nederlands", "portuguese", "pt-br" }
 	
 	local validLanguage = false
 	for k,v in ipairs(LocalTxt.Allowed) do
@@ -47,12 +78,15 @@ end
 
 function PLUGIN:LoadDefaultConfig()
 	self.Config.Language = self.Config.Language or "English"
+	self.Config.Languages = "english, german, deutsch, dutch, nederlands, portuguese, pt-br"
 	self.Config.Characters = self.Config.Characters or "abcdefghijklmnopqrstuvwxyz1234567890 [](){}!@#$%^&*_-=+.|"
-	self.Config.ReportToChat = self.Config.Characters or true
-	self.Config.AllowColorCodes = self.Config.Characters or false
-	self.Config.AllowDuplicateNames = self.Config.Characters or false
-	self.Config.AllowDuplicateNamesCaseSensitive = self.Config.Characters or true
-	self.Config.AllowBannedCharacters = self.Config.Characters or false
+	self.Config.Names = self.Config.Names or "Admin;Server;Rust;Oxide;Oxmin;Facepunch"
+	self.Config.ReportToChat = self.Config.ReportToChat or true
+	self.Config.AllowColorCodes = self.Config.AllowColorCodes or false
+	self.Config.AllowDuplicateNames = self.Config.AllowDuplicateNames or false
+	self.Config.AllowDuplicateNamesCaseSensitive = self.Config.AllowDuplicateNamesCaseSensitive or false
+	self.Config.AllowBannedCharacters = self.Config.AllowBannedCharacters or false
+	self.Config.AllowBannedNames = self.Config.AllowBannedNames or false
 end
 
 function isNumCode( num )
@@ -69,7 +103,7 @@ function PLUGIN:OnUserConnect( netuser )
 	local nameChar = false
 	local nameColor = false
 	local nameDuplicate = false
-	local duplicateUser = ""
+	local nameBanned = false
 	
 	
 	if not self.Config.AllowBannedCharacters then
@@ -97,7 +131,13 @@ function PLUGIN:OnUserConnect( netuser )
 		
 	if not self.Config.AllowDuplicateNames then
 		for k,v in ipairs(rust.GetAllNetUsers()) do
-			if ( ( netuser.displayName:lower() == v.displayName:lower() and not AllowDuplicateNamesCaseSensitive  ) or netuser.displayName == v.displayName ) and netuser ~= v then nameDuplicate = true; duplicateUser = v end
+			if ( ( name:lower() == v.displayName:lower() and not AllowDuplicateNamesCaseSensitive ) or name == v.displayName ) and netuser ~= v then nameDuplicate = true; duplicateUser = v end
+		end
+	end
+	
+	if not self.Config.AllowBannedNames then
+		for k,v in ipairs(self.Config.BannedNames) do
+			if name:lower() == v:lower() then nameBanned = true end
 		end
 	end
 	
